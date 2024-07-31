ea0801d7006c7a75ccb1e749636b0897e176b261
Mohit <5102mohit@gmail.com>
2024-07-31 16:35:49 +0530

    Initial commit

---
+++ frontend/src/utils/db/dummy.js
@@ -0,0 +1,116 @@
+export const POSTS = [
+	{
+		_id: "1",
+		text: "First time in India: Robots to clean Thiruvananthapuram International Airport",
+		img: "/posts/post1.png",
+		user: {
+			username: "johndoe",
+			profileImg: "/avatars/boy1.png",
+			fullName: "John Doe",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Amazing!",
+				user: {
+					username: "janedoe",
+					profileImg: "/avatars/girl1.png",
+					fullName: "Jane Doe",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "2",
+		text: "How you guys doing? 😊",
+		user: {
+			username: "johndoe",
+			profileImg: "/avatars/boy2.png",
+			fullName: "John Doe",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Fine ,wbu?",
+				user: {
+					username: "janedoe",
+					profileImg: "/avatars/girl2.png",
+					fullName: "Jane Doe",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "3",
+		text: "Shots fired at Donald Trump rally in Pennsylvania",
+		img: "/posts/post2.png",
+		user: {
+			username: "johndoe",
+			profileImg: "/avatars/boy3.png",
+			fullName: "John Doe",
+		},
+		comments: [],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894", "6658s895", "6658s896"],
+	},
+	{
+		_id: "4",
+		text: "Germany legend Thomas Muller to retire from international duty after crushing Euro 2024 defeat to Spain",
+		img: "/posts/post3.png",
+		user: {
+			username: "johndoe",
+			profileImg: "/avatars/boy3.png",
+			fullName: "John Doe",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Will not be forgotten!",
+				user: {
+					username: "janedoe",
+					profileImg: "/avatars/girl3.png",
+					fullName: "Jane Doe",
+				},
+			},
+		],
+		likes: [
+			"6658s891",
+			"6658s892",
+			"6658s893",
+			"6658s894",
+			"6658s895",
+			"6658s896",
+			"6658s897",
+			"6658s898",
+			"6658s899",
+		],
+	},
+];
+
+export const USERS_FOR_RIGHT_PANEL = [
+	{
+		_id: "1",
+		fullName: "AajTak",
+		username: "aajtak",
+		profileImg: "/avatars/nc1.png",
+	},
+	{
+		_id: "2",
+		fullName: "CNN",
+		username: "cnn",
+		profileImg: "/avatars/nc2.png",
+	},
+	{
+		_id: "3",
+		fullName: "NDTV",
+		username: "ndtv",
+		profileImg: "/avatars/nc3.png",
+	},
+	{
+		_id: "4",
+		fullName: "BBC",
+		username: "bbc",
+		profileImg: "/avatars/nc4.png",
+	},
+];
